61b424a1fda8c9177e703aac4e2d91ba4b08d83e
Pohzey <99220043+Pohzey@users.noreply.github.com>
2022-03-02 01:54:43 -0800

    mobile nav JS
    Need help fixing this, uploading for class on 3/3/22

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-<script></script>
+// Mobile Menu Toggle
+const toggle = document.querySelector('.toggle');
+const navbar = document.querySelector('.navbar');
+
+function toggleNavbar() {
+    if (navbar.classList.contains("active")) {
+        navbar.classList.remove("active");
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+    } else {
+        navbar.classList.add("active");
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+    }
+}
+
+toggle.addEventListener('click', toggleNavbar, false);
+
