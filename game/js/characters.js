645e36618b697b13988ee4fc7784974ef07f6c48
CullenThomas <99003985+CullenThomas@users.noreply.github.com>
2022-04-25 19:02:28 -0700

    autofocus tam
    autofocus tam

--- game/js/characters.js
+++ game/js/characters.js
@@ -1,30 +1,30 @@
-
-
 // ////////Toggles through different character divs on button click////////////////////////////
 var divs = ["tam","nii","brady","aster"];
-      let visibleId = null;
+let visibleId = null;
 
-      function start(){
-        //show("tam");
-        hide();
-      }
+// function start(){
+//   //show("tam");
+//   hide();
+// }
 
-      function show(id) {
-        if(visibleId !== id) {
-          visibleId = id;
-        } 
-        hide();
-      }
+function show(id) {
+  if(visibleId !== id) {
+    visibleId = id;
+  } 
+  hide();
+}
 
-      function hide() {
-        let div, i, id;
-        for(i = 0; i < divs.length; i++) {
-          id = divs[i];
-          div = document.getElementById(id);
-          if(visibleId === id) {
-            div.style.display = "grid";
-          } else {
-            div.style.display = "none";
-          }
-        }
-      }  
+function hide() {
+  let div, i, id;
+  for(i = 0; i < divs.length; i++) {
+    id = divs[i];
+    div = document.getElementById(id);
+    if(visibleId === id) {
+      div.style.display = "grid";
+    } else {
+      div.style.display = "none";
+    }
+  }
+}
+
+// window.onload = document.getElementById('tam-button').focus();
